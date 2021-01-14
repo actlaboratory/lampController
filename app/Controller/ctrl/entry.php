ee6cf1cb58f51671585ca42a4ae0bb5a29c7b96f
hrk-fujii <62417494+hrk-fujii@users.noreply.github.com>
2021-01-15 02:45:24 +0900

    ユーザー登録フォームを追加 (#1)
    * ユーザー登録フォームビューの作成
    
    * ユーザー登録部分の処理と表示を作成

---
+++ app/Controller/ctrl/entry.php
@@ -0,0 +1,90 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Util\ValidationUtil;
+use Model\Dao\User;
+
+use Util\CtrlUtil;
+
+$app->get("/ctrl/entry", function (request $request, response $response){
+    // ユーザ登録空フォームを表示
+    showUserEntryForm($this->view, $response);
+});
+
+$app->post("/ctrl/entry", function (request $request, response $response){
+    $inputData = $request->getParsedBody();
+    // ユーザの入力情報をバリデート
+    $errorMessage = checkUserEntryForm($inputData, $this->db);
+
+    // エラーがあれば再入力、なければ登録
+    if ($errorMessage!==""){
+        return showUserEntryForm($this->view, $response, $inputData, $errorMessage);
+    } else{
+        $softwareKey = setUserEntryForm2db($inputData, $this->db);
+        if ($softwareKey===FALSE){ // 失敗したときはやり直し
+            return showUserEntryForm($view, $response, $inputData, "登録に失敗しました。もう一度、やり直してください。");
+        } else{
+            $inputData["softwareKey"] = $softwareKey;
+            // Render view
+            return $this->view->render($response, 'ctrl/entry/success.twig', $inputData);
+        }
+    }
+});
+
+// ユーザ登録フォームを表示
+// 再表示時は、前回入力データ、エラーメッセージを元にフォームを構成
+function showUserEntryForm($view, $response, $previousData="", $errorMessage=""){
+    if (empty($previousData)){
+        $data = [];
+    } else{
+        $data = $previousData;
+    }
+    
+    $data["errorMessage"] = $errorMessage;
+
+    // Render view
+    return $view->render($response, 'ctrl/entry/index.twig', $data);
+}
+
+// ユーザ入力情報のチェック
+function checkUserEntryForm($inputData, $db){
+    $errorMessage = "";
+
+    // ユーザ名チェック
+    $errorMessage = $errorMessage. ValidationUtil::checkString("userName", $inputData["userName"], "・", "\n");
+    $userTable = new User($db);
+    $userData = $userTable->select(["user_name"=>$inputData["userName"]]);
+    if (!empty($userData)){
+        $errorMessage = $errorMessage. "・指定されたユーザー名は、すでに使用されています。\n";
+    }
+
+    // 表示名チェック
+    $errorMessage = $errorMessage. ValidationUtil::checkString("userDisplayName", $inputData["displayName"], "・", "\n");
+
+    // パスワードチェック
+    $errorMessage = $errorMessage. ValidationUtil::checkString("userPassword", $inputData["password"], "・", "\n");
+    if (!($inputData["password"]===$inputData["confirmPassword"])){
+        $errorMessage = $errorMessage. "・パスワードと、パスワードの確認が一致しません。";
+    }
+
+    return $errorMessage;
+}
+
+// ユーザ登録の書き込み。発行されたソフトウェアキーを返す
+function setUserEntryForm2db($inputData, $db){
+    $softwareKey = hash('sha256', random_int(PHP_INT_MIN, PHP_INT_MAX));
+    $userTable = new User($db);
+    $result = $userTable->insert([
+        "user_name"=> $inputData["userName"],
+        "password_hash"=> password_hash($inputData["password"], PASSWORD_DEFAULT),
+        "display_name"=> $inputData["displayName"],
+        "software_key"=> $softwareKey,
+        "last_updated_at"=> time()
+    ]);
+    if ($result===FALSE){
+        return FALSE;
+    } else{
+        return $softwareKey;
+    }
+}
--- app/Util/ValidationUtil.php
+++ app/Util/ValidationUtil.php
@@ -6,6 +6,23 @@ class ValidationUtil{
 
 	// URLの正規表現パターン
 	const URL_PATTERN = "@^https?://([a-zA-Z0-9]+[\\.\\-])+[a-zA-Z0-9]+/.*$@";
+	const USER_NAME_PATTERN = "@^[a-z0-9._\\-]{6,30}$@";
+	const USER_DISPLAY_NAME_PATTERN = "@^.{1,30}$@u";
+	const USER_PASSWORD_PATTERN = "@^[a-zA-Z0-9\\.,_\\-\\(\\)\\[\\]]{8,30}$@";
+
+	const PATTERN_ARRAY = [
+		"userName"=>[self::USER_NAME_PATTERN, "ユーザー名は、6～30文字で指定してください。英小文字、数字、記号（._-）が使用できます。"],
+		"userDisplayName"=> [self::USER_DISPLAY_NAME_PATTERN, "表示名は、1文字以上30文字以内で指定してください。"],
+		"userPassword"=>[self::USER_PASSWORD_PATTERN, "パスワードは、8文字以上30文字以内で指定してください。英数字、記号（.,_-()[]）が使用できます。大文字、小文字は区別されます。"]
+	];
+	// 文字列を検査してエラーメッセージか""を返す
+	static function checkString($key, $word, $prefix="", $sufix=""){
+		if (preg_match(self::PATTERN_ARRAY[$key][0], $word)){
+			return "";
+		} else{
+			return $prefix. self::PATTERN_ARRAY[$key][1]. $sufix;
+		}
+	}
 	
 	//必要な情報が正しい形式で含まれていることを確認する
 	static function checkParam(array $data,array $ptn){
