c458e9b1b3c58f4dad02fd51c5a8325bd0658a24
Hiroki Fujii <hfujii@hisystron.com>
2021-01-24 01:18:56 +0900

    マイページの設定項目を追加

--- app/Controller/mypage/index.php
+++ app/Controller/mypage/index.php
@@ -2,8 +2,10 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Doctrine\DBAL\Query\QueryBuilder;
 use Model\Dao\User;
 use Model\Dao\Software;
+use Model\Dao\Directory;
 use Util\SessionUtil;
 
 $app->get("/mypage", function (request $request, response $response){
@@ -14,12 +16,48 @@ $app->get("/mypage", function (request $request, response $response){
 $app->post("/mypage", function (request $request, response $response){
     $input = $request->getParsedBody();
     $message = "";
+
+    // データベース
+    $softwareTable = new Software($this->db);
+    $dirTable = new Directory($this->db);
     
     // 設定を反映
+    // 操作LAMP変更
     if (!empty($input["defaultLamp"])){
         SessionUtil::setCookie(CONFIG_COOKIE_NAME, $input["defaultLamp"]);
-        $_SESSION["defaultLamp"] = $input["defaultLamp;"];
-        $message = "捜査対象のLAMPを設定しました。";
+    $_SESSION["defaultLamp"] = $input["defaultLamp"];
+        $message = "捜査対象のLAMPを設定しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
+    }
+
+    // LAMP設定
+    if (!empty($input["manageLamp"])){
+        if ($input["manageLampType"]==="release"){
+            $softwareTable->delete([
+                "id"=> $input["manageLamp"],
+                "user_id"=> $_SESSION["userId"]
+            ]);
+            $_SESSION["defaultLamp"] = NULL;
+            $message = "LAMPの登録を解除しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
+        } elseif ($input["manageLampType"]==="name"){
+            $softwareData = $softwareTable->select([
+                "id"=> $input["manageLamp"],
+                "user_id"=> $_SESSION["userId"]
+            ]);
+            if (!empty($softwareData)){
+                $softwareTable->update([
+                    "id"=> $input["manageLamp"],
+                    "display_name"=> $input["newName"]
+                ]);
+            }
+            $message = "LAMPの名前を変更しました。";
+        }   
+        
+    }
+
+    // フォルダ削除
+    if (!empty($input["directory"])){
+        deleteDirectoryFromId($input["directory"], $this->db);
+        $message = "フォルダを削除しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
     }
     
     // マイページ再表示
@@ -44,7 +82,51 @@ function showMypage($view, $db, $response, $message=""){
     ], "display_name", "ASC", "", TRUE);
     $data["softwareList"] = $softwareData;
 
+    // ディレクトリ一覧を読み込み
+    $queryBuilder = new QueryBuilder($db);
+    $queryBuilder
+            ->select('*')
+            ->from("directory");
+    $queryBuilder->andWhere("user_id". " LIKE :user_id");
+    $queryBuilder->setParameter(":user_id", $_SESSION["userId"]);
+    $queryBuilder->andWhere("parent_id IS NULL");
+    $queryBuilder->orderBy("name", "ASC");
+    $query = $queryBuilder->execute();
+    $dirData = $query->FetchALL();
+    $data["directoryList"] = $dirData;
+
     // Render view
     return $view->render($response, 'mypage/index.twig', $data);
 }
 
+function deleteDirectoryFromId($id, $db, $parentId=NULL){
+    $dirTable = new Directory($db);
+    if ($id===NULL){
+        $dirData = $dirTable->select([
+            "parent_id"=> $parentId,
+            "user_id"=> $_SESSION["userId"]
+        ], "", "ASC", "", TRUE);
+    } else{
+        $dirData = $dirTable->select([
+            "id"=> $id,
+            "user_id"=> $_SESSION["userId"]
+        ]);
+        if (!empty($dirData)){
+            deleteDirectoryFromId(NULL, $db, $dirData["id"]);
+            $dirTable->delete([
+              "id"=> $dirData["id"]
+            ]);
+            return TRUE;
+        }
+    }
+    if (empty($dirData)){
+        return TRUE;
+    }
+    foreach($dirData as $d){
+        deleteDirectoryFromId(NULL, $db, $d["id"]);
+        $dirTable->delete([
+            "id"=> $d["id"],
+            "user_id"=> $_SESSION["userId"]
+        ]);
+    }
+}
