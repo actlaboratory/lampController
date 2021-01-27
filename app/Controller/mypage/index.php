e27f8488e3b0d8eceaea14b8653a01f7fd0d8d1d
hrk-fujii <62417494+hrk-fujii@users.noreply.github.com>
2021-01-27 18:17:21 +0900

    マイページを追加 (#5)
    * コントローラビューを実装
    
    * ステータス転送用のAPI構造を作成
    
    * ステータス情報の受信機能を追加
    
    * 時間経過表示を追加
    
    * 再生/一時停止の操作に対応
    
    * レイアウト調整
    
    * フォルダ一覧を実装
    
    * ファイル指定再生に対応
    
    * 表示の調整
    
    * &等のURLエンコード問題を修正
    
    * ソフトウェアID仮指定の解除
    
    * 表示名の表示
    
    * マイページの一部を作成
    
    * LAMP変更機能を追加
    
    * 一部ブラウザバックを無効化
    
    * マイページの設定項目を追加
    
    * mypageミドルウェアを設定
    
    * アカウント削除を実装
    
    * ユーザの表示名変更を実装
    
    * パスワードの変更機能を実装
    
    * 表示名設定にバリデーションを実施

---
+++ app/Controller/mypage/index.php
@@ -0,0 +1,139 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Model\Dao\User;
+use Model\Dao\Software;
+use Model\Dao\Directory;
+use Util\SessionUtil;
+use Util\ValidationUtil;
+
+$app->get("/mypage", function (request $request, response $response){
+    // マイページ表示
+    return showMypage($this->view, $this->db, $response);
+});
+
+$app->post("/mypage", function (request $request, response $response){
+    $input = $request->getParsedBody();
+    $message = "";
+
+    // データベース
+    $softwareTable = new Software($this->db);
+    $dirTable = new Directory($this->db);
+    
+    // 設定を反映
+    // 操作LAMP変更
+    if (!empty($input["defaultLamp"])){
+        SessionUtil::setCookie(CONFIG_COOKIE_NAME, $input["defaultLamp"]);
+    $_SESSION["defaultLamp"] = $input["defaultLamp"];
+        $message = "捜査対象のLAMPを設定しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
+    }
+
+    // LAMP設定
+    if (!empty($input["manageLamp"])){
+        if ($input["manageLampType"]==="release"){
+            $softwareTable->delete([
+                "id"=> $input["manageLamp"],
+                "user_id"=> $_SESSION["userId"]
+            ]);
+            $_SESSION["defaultLamp"] = NULL;
+            $message = "LAMPの登録を解除しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
+        } elseif ($input["manageLampType"]==="name"){
+            $message = ValidationUtil::checkString("lampDisplayName", $input["newName"]);
+            if (empty($message)){
+                $softwareData = $softwareTable->select([
+                    "id"=> $input["manageLamp"],
+                    "user_id"=> $_SESSION["userId"]
+                ]);
+                if (!empty($softwareData)){
+                    $softwareTable->update([
+                        "id"=> $input["manageLamp"],
+                        "display_name"=> $input["newName"]
+                    ]);
+                    $message = "LAMPの名前を変更しました。";
+                } else{
+                    $message = "LAMPの名前を変更できませんでした。";
+                }
+            }
+
+        }   
+        
+    }
+
+    // フォルダ削除
+    if (!empty($input["directory"])){
+        deleteDirectoryFromId($input["directory"], $this->db);
+        $message = "フォルダを削除しました。\nブラウザの戻る機能を利用する場合は、移動先のページを再読み込みしてください。";
+    }
+    
+    // マイページ再表示
+    return showMypage($this->view, $this->db, $response, $message);
+});
+
+// マイページを表示
+function showMypage($view, $db, $response, $message=""){
+    $data = ["message"=> $message];
+    
+    // ユーザー情報を提示
+    $userTable = new User($db);
+    $userData = $userTable->select([
+        "id"=> $_SESSION["userId"]
+    ]);
+    $data["userDisplayName"] = $userData["display_name"];
+
+    // LAMP一覧を読み込み
+    $softwareTable = new Software($db);
+    $softwareData = $softwareTable->select([
+        "user_id"=> $_SESSION["userId"]
+    ], "display_name", "ASC", "", TRUE);
+    $data["softwareList"] = $softwareData;
+
+    // ディレクトリ一覧を読み込み
+    $queryBuilder = new QueryBuilder($db);
+    $queryBuilder
+            ->select('*')
+            ->from("directory");
+    $queryBuilder->andWhere("user_id". " LIKE :user_id");
+    $queryBuilder->setParameter(":user_id", $_SESSION["userId"]);
+    $queryBuilder->andWhere("parent_id IS NULL");
+    $queryBuilder->orderBy("name", "ASC");
+    $query = $queryBuilder->execute();
+    $dirData = $query->FetchALL();
+    $data["directoryList"] = $dirData;
+
+    // Render view
+    return $view->render($response, 'mypage/index.twig', $data);
+}
+
+function deleteDirectoryFromId($id, $db, $parentId=NULL){
+    $dirTable = new Directory($db);
+    if ($id===NULL){
+        $dirData = $dirTable->select([
+            "parent_id"=> $parentId,
+            "user_id"=> $_SESSION["userId"]
+        ], "", "ASC", "", TRUE);
+    } else{
+        $dirData = $dirTable->select([
+            "id"=> $id,
+            "user_id"=> $_SESSION["userId"]
+        ]);
+        if (!empty($dirData)){
+            deleteDirectoryFromId(NULL, $db, $dirData["id"]);
+            $dirTable->delete([
+              "id"=> $dirData["id"]
+            ]);
+            return TRUE;
+        }
+    }
+    if (empty($dirData)){
+        return TRUE;
+    }
+    foreach($dirData as $d){
+        deleteDirectoryFromId(NULL, $db, $d["id"]);
+        $dirTable->delete([
+            "id"=> $d["id"],
+            "user_id"=> $_SESSION["userId"]
+        ]);
+    }
+}
--- app/Util/ValidationUtil.php
+++ app/Util/ValidationUtil.php
@@ -4,16 +4,18 @@ namespace Util;
 
 class ValidationUtil{
 
-	// URLの正規表現パターン
+	// 各種文字列の正規表現パターン
 	const URL_PATTERN = "@^https?://([a-zA-Z0-9]+[\\.\\-])+[a-zA-Z0-9]+/.*$@";
 	const USER_NAME_PATTERN = "@^[a-z0-9._\\-]{6,30}$@";
 	const USER_DISPLAY_NAME_PATTERN = "@^.{1,30}$@u";
 	const USER_PASSWORD_PATTERN = "@^[a-zA-Z0-9\\.,_\\-\\(\\)\\[\\]]{8,30}$@";
+	const LAMP_DISPLAY_NAME_PATTERN = "@^.{1,30}$@u";
 
 	const PATTERN_ARRAY = [
 		"userName"=>[self::USER_NAME_PATTERN, "ユーザー名は、6～30文字で指定してください。英小文字、数字、記号（._-）が使用できます。"],
 		"userDisplayName"=> [self::USER_DISPLAY_NAME_PATTERN, "表示名は、1文字以上30文字以内で指定してください。"],
-		"userPassword"=>[self::USER_PASSWORD_PATTERN, "パスワードは、8文字以上30文字以内で指定してください。英数字、記号（.,_-()[]）が使用できます。大文字、小文字は区別されます。"]
+		"userPassword"=>[self::USER_PASSWORD_PATTERN, "パスワードは、8文字以上30文字以内で指定してください。英数字、記号（.,_-()[]）が使用できます。大文字、小文字は区別されます。"],
+		"lampDisplayName"=> [self::USER_DISPLAY_NAME_PATTERN, "表示名は、1文字以上30文字以内で指定してください。"]
 	];
 	// 文字列を検査してエラーメッセージか""を返す
 	static function checkString($key, $word, $prefix="", $sufix=""){
