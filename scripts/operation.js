c40f78cf6c69c7a43b8e06d5d5c31d35a87d57a7
Hiroki Fujii <hfujii@hisystron.com>
2021-02-07 01:16:37 +0900

    コントローラボタンのUI改善

--- scripts/operation.js
+++ scripts/operation.js
@@ -1,4 +1,13 @@
 function operation(name){
+    if (softwareId == null){
+        alert("操作対象のLAMPが設定されていません。マイページトップより、操作対象のLAMPを設定してください。");
+        return;
+    }
+    if (softwareStatus.innerText == "未接続"){
+        alert("現在、LAMPと接続されていません。ソフトウェアの起動と、設定を確認してください。\nマイページトップより、操作対象のLAMPを変更することもできます。");
+        return;
+    }
+    
     var reqArray = {
         "sessionId": sessionId,
         "softwareId": softwareId,
