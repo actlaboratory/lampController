b47833ec9b237f1a908f21793249a979b9877aa8
Hiroki Fujii <hfujii@hisystron.com>
2021-02-23 23:22:18 +0900

    無効なコントローラ操作のエラーメッセージを変更

--- scripts/operation.js
+++ scripts/operation.js
@@ -1,10 +1,10 @@
 function operation(name){
     if (softwareId == null){
-        alert("操作対象のLAMPが設定されていません。マイページトップより、操作対象のLAMPを設定してください。");
+        alert("操作対象のLAMPが設定されていません。");
         return;
     }
     if (softwareStatus.innerText == "未接続"){
-        alert("現在、LAMPと接続されていません。ソフトウェアの起動と、設定を確認してください。\nマイページトップより、操作対象のLAMPを変更することもできます。");
+        alert("現在、LAMPと接続されていません。ソフトウェアの起動と、設定を確認してください。\nまたは、操作対象のLAMPを変更してください。");
         return;
     }
     
